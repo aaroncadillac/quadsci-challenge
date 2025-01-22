136e4e028723b1a8e12de58618ae11fec10e07be
Aarón Gabriel Hinojosa Maya <aaron.hmaya@gmail.com>
2025-01-22 01:43:45 -0600

    feat: split google apis into another tf file

--- setup/main.tf
+++ setup/main.tf
@@ -23,11 +23,6 @@ variable "gke_service_account_name" {
   type = string
 }
 
-variable "gcp_service_list" {
-  description ="The list of apis necessary for the project"
-  type = list(string)
-}
-
 variable "project_name" {
   type = string
 }
@@ -44,12 +39,6 @@ provider "google" {
   credentials = file("quadsci-access.json")
   project = "quadsci-exercise-aaron"
   region  = var.region
-}
-
-resource "google_project_service" "gcp_services" {
-  for_each = toset(var.gcp_service_list)
-  project = var.project_name
-  service = each.key
 }
 
 resource "google_workbench_instance" "vertex_instance" {
