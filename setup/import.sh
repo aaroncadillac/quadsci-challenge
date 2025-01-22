0fa6cac3e0d9683161fd6e99aebff2e9b7aae506
Aarón Gabriel Hinojosa Maya <aaron.hmaya@gmail.com>
2025-01-22 01:48:18 -0600

    fix: change import for main.tf

--- setup/import.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-terraform import google_project_service.gcp_services["cloudresourcemanager.googleapis.com"] quadsci-exercise-aaron/cloudresourcemanager.googleapis.com && \
-terraform import google_project_service.gcp_services["compute.googleapis.com"] quadsci-exercise-aaron/compute.googleapis.com && \
-terraform import google_project_service.gcp_services["container.googleapis.com"] quadsci-exercise-aaron/container.googleapis.com
